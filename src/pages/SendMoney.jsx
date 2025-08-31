57111720b0590026cf93ab4e16f43bd939fd3aa6
SafalBhandari12 <safalbhandari069@gmail.com>
2025-09-01 01:19:14 +0530

    added transfer component

--- src/pages/SendMoney.jsx
+++ src/pages/SendMoney.jsx
@@ -34,7 +34,10 @@ export const SendMoney = () => {
                   placeholder='Enter amount'
                 />
               </div>
-              <button class='justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white'>
+              <button
+                onClick={() => {}}
+                class='justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white'
+              >
                 Initiate Transfer
               </button>
             </div>
